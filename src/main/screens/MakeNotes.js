e94503ca633132a23281974cb4ca9306f719aeb1
Usnagiri Srikanth <UsnagiriS@jmf.com>
2023-09-05 15:28:50 +0530

    make notes

--- src/main/screens/MakeNotes.js
+++ src/main/screens/MakeNotes.js
@@ -1,11 +1,41 @@
 import {View, Text} from 'react-native';
 import React from 'react';
+import {SafeAreaView} from 'react-native';
 
 const MakeNotes = () => {
   return (
-    <View>
-      <Text>MakeNotes</Text>
-    </View>
+    <SafeAreaView
+      style={{
+        flex: 1,
+      }}>
+      <View
+        style={{
+          flex: 1,
+        }}>
+        <View
+          style={{
+            alignItems: 'center',
+            backgroundColor: '#fff',
+            paddingVertical: 10,
+          }}>
+          <Text
+            style={{
+              fontSize: 22,
+              fontWeight: 'bold',
+              lineHeight: 29,
+              color: '#000000',
+            }}>
+            MakeNotes
+          </Text>
+        </View>
+        <View
+          style={{
+            flex: 1,
+            backgroundColor: '#f5fafc',
+            paddingHorizontal: 20,
+          }}></View>
+      </View>
+    </SafeAreaView>
   );
 };
 
